8455a084ca92b40ecf0ca5d79832d11ce00bd006
ae243 <annee@cs.princeton.edu>
2018-05-31 14:17:43 -0400

    update similarity script

--- root_data_analysis/apply_similarity.py
+++ root_data_analysis/apply_similarity.py
@@ -1,5 +1,5 @@
 import sys
-from Levenshtein import jaro
+from Levenshtein import jaro_winkler
 
 f = open(sys.argv[1], 'r')
 names = []
@@ -9,16 +9,19 @@ f.close()
 
 count = 0
 pairs = []
+all_pairs = []
 for n in names:
     print "Completed: " + str(count)
     for n2 in names:
-        x = jaro(n, n2)
+        x = jaro_winkler(n, n2)
         if x > .90 and n != n2 and ((n,n2) not in pairs) and ((n2,n) not in pairs):
             pairs.append((n,n2))
     count += 1
 
+print len(all_pairs)
+
 f = open(sys.argv[2], 'w')
 for p in pairs:
-    f.write(p[0] + "," + p[1] + "," + str(jaro(p[0], p[1])) + "\n")
+    f.write(p[0] + "," + p[1] + "," + str(jaro_winkler(p[0], p[1])) + "\n")
 f.close()
 
